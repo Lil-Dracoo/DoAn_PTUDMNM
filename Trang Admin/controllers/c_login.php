cca08258278e457a934a5acf67909489a1f9cedb
Du Cuong <ducuong133@gmail.com>
2025-12-15 14:34:19 +0700

    feat: add login controller

---
+++ Trang Admin/controllers/c_login.php
@@ -0,0 +1,26 @@
+<?php
+// Kiểm tra nếu người dùng bấm nút Đăng nhập
+if (isset($_POST['dangnhap']) && ($_POST['dangnhap'])) {
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+
+    // Hàm check_tk đã được include ở index.php nên dùng được luôn
+    $user_info = check_tk($user, $pass);
+
+    if ($user_info && is_array($user_info)) {
+        if ($user_info['vai_tro'] == 1 || $user_info['vai_tro'] == 2) {
+            $_SESSION['user1'] = $user_info;
+            // Đăng nhập thành công -> Load lại trang chủ
+            header('location: index.php');
+            exit;
+        } else {
+            $loi = "Tài khoản không có quyền truy cập!";
+        }
+    } else {
+        $loi = "Sai tên đăng nhập hoặc mật khẩu!";
+    }
+}
+
+// Gọi giao diện đăng nhập (đường dẫn tính từ index.php)
+include "./view/login.php";
+?>
